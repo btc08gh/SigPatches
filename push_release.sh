954fd5e7d9b51efe32de29c81ebde90eef7e6cbc
borntohonk <6264306+borntohonk@users.noreply.github.com>
2021-10-17 13:45:49 +0000

    Tweak auto-published changelogs to pull proper latest supported fw version rather than whatever Nintendo says is latest.

--- push_release.sh
+++ push_release.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 HASH=$(curl --silent "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases" | grep "browser_download_url" | head -1 | grep -oE "\-([0-9a-f]{8})" | cut -c2-)
-AMSVERSION=$(curl --silent "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases" | grep "tag" | head -1 | cut -c 75-79)
-FWVERSION=$(curl --silent "https://en-americas-support.nintendo.com/app/answers/detail/a_id/22525/kw/nintendo%20switch%20system%20update" | grep "Ver." | head -1 | cut -c 28-33)
+AMSMAJORVER=$(grep 'define ATMOSPHERE_RELEASE_VERSION_MAJOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3)
+AMSMINORVER=$(grep 'define ATMOSPHERE_RELEASE_VERSION_MINOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3)
+AMSMICROVER=$(grep 'define ATMOSPHERE_RELEASE_VERSION_MICRO\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3)
+HOS_MAJORVER=$(grep 'define ATMOSPHERE_SUPPORTED_HOS_VERSION_MAJOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3)
+HOS_MINORVER=$(grep 'define ATMOSPHERE_SUPPORTED_HOS_VERSION_MINOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3)
+HOS_MICROVER=$(grep 'define ATMOSPHERE_SUPPORTED_HOS_VERSION_MICRO\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3)
+HOSVER=$HOS_MAJORVER.$HOS_MINORVER.$HOS_MICROVER
+AMSVER=$AMSMAJORVER.$AMSMINORVER.$AMSMICROVER
 res=`curl --user "borntohonk:$GITHUB_TOKEN" -X POST https://api.github.com/repos/borntohonk/patches/releases \
 -d "
 {
-    \"tag_name\": \"$FWVERSION-$AMSVERSION-$HASH\",
+    \"tag_name\": \"$HOSVER-$AMSVER-$HASH\",
     \"target_commitish\": \"master\",
-    \"name\": \"Supports HOS firmware $FWVERSION and AMS $AMSVERSION-$HASH\",
-    \"body\": \"- es, fs and nifm patches for HOS ${FWVERSION} are included. \r\n\r\n- has loader patches for Atmosphere version ${AMSVERSION}-${HASH} \r\n\r\n- Hekate style patches are included for both loader and FS \",
+    \"name\": \"Supports HOS firmware $HOSVER and AMS $AMSVER-$HASH\",
+    \"body\": \"- es, fs and nifm patches for HOS ${HOSVER} are included. \r\n\r\n- has loader patches for Atmosphere version ${AMSVER}-${HASH} \r\n\r\n- Hekate style patches are included for both loader and FS \",
     \"draft\": false,
     \"prerelease\": false
 }"`
