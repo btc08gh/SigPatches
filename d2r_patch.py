b97edaeeddcc6fa62f037275a3913f21f94b6b61
borntohonk <6264306+borntohonk@users.noreply.github.com>
2021-10-22 08:17:21 +0000

    Add d2r patch for version 1.0.0.6 / v393216

--- d2r_patch.py
+++ d2r_patch.py
@@ -7,7 +7,7 @@ def get_build_id():
 
 with open('main', 'rb') as fi:
     read_data = fi.read()
-    result = re.search(b'\xb9...\x91...\x78...\xf9...\x94...\x36...\x94...\xf9...\xd2...\xf2...\xf2...\xcb...\xeb.......\x14', read_data)
+    result = re.search(b'\xd2...\xf2...\xf2...\xcb...\xeb.......\x14', read_data)
     patch = "%06X%s%s" % (result.end(), "0004", "1F2003D5")
     text_file = open(get_build_id() + ".ips", "wb")
     print("d2r build-id: " + get_build_id())
