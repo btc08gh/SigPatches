c7aca33d05ddeaf0913c04fae848e5c687b783c6
borntohonk <6264306+borntohonk@users.noreply.github.com>
2021-10-23 14:32:02 +0000

    add script to port-forward dvr patches

--- bundle_patches.sh
+++ bundle_patches.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir bootloader && \
 ls -1t hekate_patches/*.ini | while read fn ; do cat "$fn" >> bootloader/patches.ini; done && \
-zip -r patches.zip ./atmosphere ./bootloader -x './atmosphere/contents/*' && \
+zip -r patches.zip ./atmosphere ./bootloader -x './atmosphere/contents/*' -x './atmosphere/exefs_patches/am/*' && \
 rm -rf bootloader
